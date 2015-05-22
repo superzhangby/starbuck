332709b417a4a825b5f5a3fc676b5bf6560a137b
Iris-lsy <iris.lsy.thy@gmail.com>
2015-05-22 17:29:38 -0400

    read in

--- src/controller/ReadAction.java
+++ src/controller/ReadAction.java
@@ -10,6 +10,9 @@
  */
 package controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +21,9 @@ import javax.servlet.http.HttpSession;
 import model.Model;
 import model.MyDAOException;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.mybeans.form.FormBeanException;
 import org.mybeans.form.FormBeanFactory;
 
@@ -65,13 +71,68 @@ public class ReadAction extends Action {
 		}
 
 		if (form.getAction().equals("readin")) {
-			
-			
-			
+			File file = null;
+			int maxFileSize = 5000 * 1024;
+			int maxMemSize = 5000 * 1024;
+
+			String filePath = "/Users/LEE45/Documents/apache-tomcat-7.0.56/data/";
+			String contentType = request.getContentType();
+			if ((contentType.indexOf("multipart/form-data") >= 0)) {
+				DiskFileItemFactory factory = new DiskFileItemFactory();
+				factory.setSizeThreshold(maxMemSize);
+				factory.setRepository(new File("/Users/LEE45/Documents/temp"));
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				upload.setSizeMax(maxFileSize);
+				try {
+					List fileItems = upload.parseRequest(request);
+					Iterator i = fileItems.iterator();
+
+					while (i.hasNext()) {
+						FileItem fi = (FileItem) i.next();
+						if (!fi.isFormField()) {
+
+							String fieldName = fi.getFieldName();
+							String fileName = fi.getName();
+							boolean isInMemory = fi.isInMemory();
+							long sizeInBytes = fi.getSize();
+
+							if (fileName.lastIndexOf("\\") >= 0) {
+								file = new File(filePath,
+										fileName.substring(fileName
+												.lastIndexOf("\\")));
+							} else {
+								file = new File(filePath,
+										fileName.substring(fileName
+												.lastIndexOf("\\") + 1));
+							}
+							fi.write(file);
+
+						}
+					}
+
+					BufferedReader reader = null;
+					reader = new BufferedReader(new FileReader(file));
+					String tempString = null;
+					while ((tempString = reader.readLine()) != null) {
+						// TODO 将内容转化成JSON
+
+						System.out.println(tempString);
+					}
+					reader.close();
+
+				} catch (Exception ex) {
+					System.out.println(ex);
+				}
+			} else {
+
+				// TODO 加error
+
+			}
+
 			return "index.html";
 		}
 		if (form.getAction().equals("Login as Employee")) {
-			
+
 		}
 		return "login.jsp";
 	}
