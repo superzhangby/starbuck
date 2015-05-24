78be3c3436ceaa159d6d1a81349bca46f4d9f9f1
maojiewen <maojiewen@maojiewens-air.wv.cc.cmu.edu>
2015-05-24 14:40:09 -0400

    read in

---
+++ src/controller/DataFormAction.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import model.Model;
+
+import org.mybeans.form.FormBeanFactory;
+
+import form.DataForm;
+
+public class DataFormAction extends Action{
+	private FormBeanFactory<DataForm> formBeanFactory = FormBeanFactory
+			.getInstance(DataForm.class);
+
+	public DataFormAction(Model model) {
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return "index.jsp";
+	}
+}
